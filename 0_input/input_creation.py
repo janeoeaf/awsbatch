9ee4ac92b27aa181e1a2008892d85c57cf684225
janeoeaf <janeoeaf@gmail.com>
2022-04-20 21:41:46 -0300

    make input script is done

---
+++ 0_input/input_creation.py
@@ -0,0 +1,42 @@
+from io import StringIO
+import pandas as pd
+import boto3
+import os
+
+#---provide the aws credentials or create env variables
+#os.environ["ACCESS_KEY"]="??"
+#os.environ["SECRET_KEY"]="??"
+
+
+#--- create input data frame
+n=1000
+colA=[]
+colB=[]
+for i in range(n):
+    colA.append('A'+str(i))
+    colB.append('B'+str(n-i-1))
+
+di={
+    'job':range(n),
+    'colA':colA,
+    'colB':colB
+}
+
+df=pd.DataFrame.from_dict(di)
+
+
+
+bucket="batch-example"
+
+client=boto3.client('s3')
+
+
+csv_buffer=StringIO()
+df.to_csv(csv_buffer,index=False,encoding='utf-8')
+
+response=client.put_object(
+    ACL='private',
+    Body=csv_buffer.getvalue(),
+    Bucket=bucket,
+    Key='input/input.csv'
+)
